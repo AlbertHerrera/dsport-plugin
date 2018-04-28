b4cd08f46f56f9e661e50face51ae0f764963214
Albert Herrera Castel <castelherreraalbert@gmail.com>
2018-04-28 04:10:59 +0200

    Added first necessary coments

--- dsports.php
+++ dsports.php
@@ -1,1 +1,12 @@
-dad mom
+<?php
+/**
+ * Plugin Name: Dsports
+ * Plugin URI: https://github.com/AlbertHerrera/dsport_plugin
+ * Description: Este plugin crea concursos online.
+ * Version: 0.0.1
+ * Author: Albert Herrera
+ * Author URI: wwww.shuffleshowcase.com
+ *
+ * Text Domain: dsports
+ */
+defined( 'ABSPATH' ) or die( '¡Sin trampas!' );
