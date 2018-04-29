d4631edf6bbe36f1acd327dd9643e46811ea08a3
Albert Herrera Castel <castelherreraalbert@gmail.com>
2018-04-29 15:35:29 +0200

    try2

--- dsports-plugin.php
+++ dsports-plugin.php
@@ -22,7 +22,7 @@ of the License, or (at your option) any later version.
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.ss
+GNU General Public License for more details.ssss
 
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
