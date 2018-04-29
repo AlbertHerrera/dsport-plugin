bef60699a607f557f8911810eee3728c9f8102e9
Albert Herrera Castel <castelherreraalbert@gmail.com>
2018-04-29 15:51:05 +0200

    Deleted origin?

--- dsports-plugin.php
+++ dsports-plugin.php
@@ -22,7 +22,7 @@ of the License, or (at your option) any later version.
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.ssssffff
+GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
